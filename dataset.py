bdea854e48201303a83d5e0bb861ec36cc026ca0
i4ata <ivaylo.russinov@gmail.com>
2024-03-10 13:25:48 +0100

    updates

--- dataset.py
+++ dataset.py
@@ -1,6 +1,59 @@
 from torchvision.datasets import VOCDetection
+from torch.utils.data import Dataset, DataLoader
+import albumentations as A
+from typing import Tuple, List
+import numpy as np
+import torch
+from tqdm import tqdm
 
-print('dataset imported')
-dataset = VOCDetection('val', image_set='val', download=True)
-print(dataset)
-print(dataset[0])
+class YOLODataset(Dataset):
+    def __init__(self, root: str = 'train') -> None:
+        super().__init__()
+        self.pascal_voc = VOCDetection(root, year='2007', image_set=root)
+        self.image_size = 448
+        self.transform = A.Compose(
+            [A.Resize(self.image_size, self.image_size)],
+            bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'])
+        )
+        self.classes = ['person', 
+                        'bird', 'cat', 'cow', 'dog', 'horse', 'sheep',
+                        'aeroplane', 'bicycle', 'boat', 'bus', 'car', 'motorbike', 'train',
+                        'bottle', 'chair', 'diningtable', 'pottedplant', 'sofa', 'tvmonitor']
+
+    def __len__(self) -> int:
+        return len(self.pascal_voc)
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor, torch.Tensor]]:
+        image, annotations = self.pascal_voc[index]
+        boxes, labels = [], []
+        
+        for object in annotations['annotation']['object']:
+            boxes.append(list(map(int, object['bndbox'].values())))
+            labels.append(object['name'])
+        
+        transform = self.transform(image=np.asarray(image), bboxes=boxes, labels=labels)
+
+        # Setting up the image
+        image = torch.tensor(transform['image']) / 255.
+
+        # Setting up the targets
+        # Transform boxes from [x_min, y_min, x_max, y_max] to [x_center, y_center, width, height]
+        boxes = torch.tensor(transform['bboxes']).float()
+        x, y = (boxes[:, 0] + boxes[:, 2]) / 2, (boxes[:, 1] + boxes[:, 3]) / 2
+        w, h =  boxes[:, 2] - boxes[:, 0],       boxes[:, 3] - boxes[:, 1]
+
+        objectness_score = torch.ones(len(boxes))
+        labels = torch.tensor(list(map(self.classes.index, transform['labels'])))
+
+        feature_map_dim = self.image_size // 7
+        feature_map = torch.zeros(feature_map_dim, feature_map_dim, 6)
+        feature_map[(x // 7).long(), (y // 7).long()] = torch.stack(
+            (x, y, w, h, objectness_score, labels), dim=1
+        )
+
+        return image, feature_map
+
+if __name__ == '__main__':
+    test_dataset = YOLODataset('test')
+    for i in tqdm(range(len(test_dataset))):
+        a = test_dataset[i]
---
+++ fast_yolov1.py
@@ -0,0 +1,44 @@
+# https://github.com/pjreddie/darknet/blob/master/cfg/yolov1-tiny.cfg
+
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(ConvBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.LeakyReLU(negative_slope=.1),
+        )
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.block(x)
+
+class FastYOLO1(nn.Module):
+    def __init__(self) -> None:
+        super(FastYOLO1, self).__init__()
+        
+        pool = nn.MaxPool2d(2)
+        self.net = nn.Sequential(
+            ConvBlock(3, 16), pool, 
+            ConvBlock(16, 32), pool,
+            ConvBlock(32, 64), pool,
+            ConvBlock(64, 128), pool,
+            ConvBlock(128, 256), pool,
+            ConvBlock(256, 512), pool,
+            ConvBlock(512, 1024),
+            ConvBlock(1024, 256),
+            nn.Flatten(),
+            nn.Linear(in_features=256 * 7 * 7, out_features=1470)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.net(x).view(len(x), 7, 7, -1)
+    
+if __name__ == '__main__':
+    yolo = FastYOLO1()
+    im = torch.rand(5,3,448,448)
+    out = yolo(im)
+    print(sum(p.numel() for p in yolo.parameters()))
+    print(out.shape)
+    # print(yolo)
